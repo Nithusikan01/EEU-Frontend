499f207ea18628d928e14597208362d61531858c
Danujan <sivasundaralingamdanujan@gmail.com>
2023-10-11 22:09:49 +0530

    Updated

--- src/components/menu/RoutesConfig.js
+++ src/components/menu/RoutesConfig.js
@@ -6,14 +6,16 @@ import Offline from '../../Pages/Offline';
 import Contact from '../../Pages/Contact';
 import Home from '../../Pages/Home';
 import Development from '../../Pages/Development/Development';
-{/*import Seminarroom1 from "../../Pages/5_Facilities/Seminarroom1"
+import Teal1 from "../../Pages/6_Teal/Teal1"
+import Teal2 from "../../Pages/6_Teal/Teal2"
+import Seminarroom1 from "../../Pages/5_Facilities/Seminarroom1"
+{/*"
 import Seminarroom2 from "../../Pages/5_Facilities/Seminarroom2"
 import Seminarroom3 from './../../Pages/5_Facilities/Seminarroom3';
 import AVRecordingRoom from "./../../Pages/5_Facilities/AVRecordingRoom"
 import ConferenceRoom from "./../../Pages/5_Facilities/ConferenceRoom"
 import LectureRooms from '../../Pages/5_Facilities/LectureRooms';
-import Teal1 from "../../Pages/6_Teal/Teal1"
-import Teal2 from "../../Pages/6_Teal/Teal2"*/}
+*/}
 
 
 
@@ -28,15 +30,16 @@ const RoutesConfig = () => {
       <Route path="/Offline" element={<Offline />} />
       <Route path="/Contact" element={<Contact />} />
       <Route path="/Development" element={<Development/>} />
-      {/*<Route path="/Seminarroom1" element={<Seminarroom1/>} />
+      <Route path="/Teal1" element={<Teal1/>} />
+      <Route path="/Teal2" element={<Teal2/>} />
+      <Route path="/Seminarroom1" element={<Seminarroom1/>} />
+      {/*
       <Route path="/Seminarroom2" element={<Seminarroom2/>} />
       <Route path="/Seminarroom3" element={<Seminarroom3/>} />
       <Route path="/AVRecordingRoom" element={<AVRecordingRoom/>} />
       <Route path="/ConferenceRoom" element={<ConferenceRoom/>} />
       <Route path="/LectureRooms" element={<LectureRooms/>} />
-      
-      <Route path="/Teal1" element={<Teal1/>} />
-  <Route path="/Teal2" element={<Teal2/>} />*/}
+      */}
     </Routes>
   );
 };
